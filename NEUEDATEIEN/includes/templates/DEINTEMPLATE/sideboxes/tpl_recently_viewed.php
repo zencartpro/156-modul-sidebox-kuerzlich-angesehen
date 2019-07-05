d180635eb40b595af9fb8be81f643c1fcf08c1e4
webchills <office@webchills.com>
2019-07-05 16:28:21 +0200

    initial commit

---
+++ NEUEDATEIEN/includes/templates/DEINTEMPLATE/sideboxes/tpl_recently_viewed.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Side Box Template
+ *
+ * @package templateSystem
+ * @copyright Copyright 2003-2018 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
+ * @version $Id: tpl_recently_viewed.php 2018-10-25 18:56:41Z webchills $
+ */
+  $content = '';
+  $content .= '<div id="' . str_replace('_', '-', $box_id . 'Content') . '" class="sideBoxContent">' . "\n";
+  $content .= '<div class="wrapper">' . "\n";
+  $content .= '<table class="recentlyviewed">' . "\n";
+  foreach ($_SESSION['recent_products'] as $recent_product) {
+	$link = zen_href_link(zen_get_info_page($recent_product), 'products_id=' . $recent_product);
+	$content .= '<tr>' . "\n" . 
+				  '<td></td>' . "\n" . 
+				  '<td><div class="recent-products-name"><a href="' . $link . '">' . zen_get_products_name($recent_product, $_SESSION['languages_id']) . '</a></div></td>' . "\n" . 
+				 '</tr>';
+  }
+  $content .= '</table>' . "\n";
+  $content .= '</div>' . "\n";
+  $content .= '</div>';
